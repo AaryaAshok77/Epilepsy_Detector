3510c488d34e91374c3c047e9b67e66f6ab7f184
Aarya Ashok K <aaryaashokk77@gmail.com>
2024-03-01 18:24:05 +0530

    Loaded data and created their labeled list

--- test.py
+++ test.py
@@ -8,3 +8,19 @@ edf_files = glob(directory + '*.edf')
 edf_files = [file for file in edf_files if not file.endswith('.seizure.edf')]
 
 print(edf_files)
+
+import torch
+import numpy as np
+import pandas as pd
+import sklearn
+import matplotlib.pyplot as plt
+
+print(f"PyTorch version: {torch.__version__}")
+
+# Check PyTorch has access to MPS (Metal Performance Shader, Apple's GPU architecture)
+print(f"Is MPS (Metal Performance Shader) built? {torch.backends.mps.is_built()}")
+print(f"Is MPS available? {torch.backends.mps.is_available()}")
+
+# Set the device      
+device = "mps" if torch.backends.mps.is_available() else "cpu"
+print(f"Using device: {device}")
